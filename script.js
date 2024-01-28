695dfd0f9d6f4372725c71a21108809c276038e3
sam <shivambisht267@gmail.com>
2024-01-28 09:28:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var arr = [
+    {dp:"https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",stor:"https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"},
+    {dp:"https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",stor:"https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"},
+    {dp:"https://images.unsplash.com/photo-1549545931-59bf067af9ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1430&q=80",stor:"https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"},
+    {dp:"https://images.unsplash.com/photo-1541832186590-2bffabbc08db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1579&q=80",stor:"https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"},
+]
+ 
+var story = document.querySelector("#story");
+
+var cultr = "";
+arr.forEach(function(elm,idx){
+    cultr += `<div class="stor">
+    <img id="${idx}" src="${elm.dp}" alt="">
+</div>`
+})
+
+
+story.innerHTML = cultr;
+
+story.addEventListener("click",function(dt){
+    document.querySelector("#full-screen").style.display = "block";
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dt.target.id].stor})`;
+
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display = "none";
+    },3000)
+});
